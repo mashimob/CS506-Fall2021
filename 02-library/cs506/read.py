93cae1bfcf9fc6560cb3b701481f49997ea09bcf
Bryce Mashimo <mashimob@bu.edu>
2021-10-01 15:07:10 -0400

    returning matrix for read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,12 @@
+import numpy
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    matrix = numpy.array(list(csv.reader(open("test.csv", "rb"), delimiter=","))).astype("float")
+
+
+    return matrix
+
